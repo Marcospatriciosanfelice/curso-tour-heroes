fcd5376dfbaa5708107d3d6f3ddd07792f1fc25c
Marcospatriciosanfelice <marcos.sanfelice@gmail.com>
2022-05-17 10:06:28 -0300

    curso angular

---
+++ curso/src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { identifierName } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent  {
+
+  hero: Hero = {
+
+   id: 1,
+   name: 'Batman'
+
+   };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+
+ 
+   onSelect(hero: Hero): void {
+
+    
+    this.selectedHero = hero;
+
+
+
+
+   }
+}
---
+++ curso/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+{  id: 1, name: 'Hulk'},
+{  id: 2, name: 'Tannos'},
+{  id: 3, name: 'Aranha'},
+{  id: 4, name: 'Visao'},
+{  id: 5, name: 'Wolverine'},
+{  id: 6, name: 'Batman'},
+];
